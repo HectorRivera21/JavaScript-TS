cc7a7dc36c6aae317d06a4a18901b6ed00bcf0fb
HectorRivera <redmagix2000@gmail.com>
2023-06-07 04:23:01 -0500

    NA

--- discordBot/Projects/projectbot/index.mjs
+++ discordBot/Projects/projectbot/index.mjs
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 dotenv.config();
-import { Client, IntentsBitField, } from 'discord.js';
+import { Client, IntentsBitField, EmbedBuilder} from 'discord.js';
 // import cron from 'node-cron';
+const prefix = "!"
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -15,12 +16,22 @@ client.login(process.env.DISCORD_TOKEN);
 client.on('ready',(c)=>{
     console.log(`${c.user.tag} is ready`);
 });
-try {
-	const response = await fetch("http://animechan.melosh.space/random")
-    const result = await response.json();
-    console.log(result);
-	
-} catch (error) {
-	console.error(error);
-}
+client.on('messageCreate', async (msg) => {
+    if(msg.content.startsWith(prefix + 'Q') && msg.author.bot != true){
+        try {
+            var response = await fetch("http://animechan.melosh.space/random")
+            var random = await response.json();
+            console.log(result);
+        } catch (error) {
+            console.error(error);
+        }
+        const exampleEmbed = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle(`${random.anime}`)
+        .setAuthor({name: `${random.character}`})
+        .setDescription(`${random.quote}`);
 
+        const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+        channel.send({ embeds: [exampleEmbed] });
+    };
+});
